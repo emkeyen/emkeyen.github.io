fdcefe3cb0f28b37a7f4b84826b2e3bd908de719
emkeyen <emkeyen96@gmail.com>
2025-05-31 13:59:55 +0200

    first commit

---
+++ content/test.md
@@ -0,0 +1,5 @@
+Title: Custom HTML pytest reporter
+Date: 2025-05-31
+Tags: Python, pytest, Testing, Automation
+Category: Testing
+Summary: Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nullam dignissim convallis est. Quisque aliquam. Donec faucibus. Nunc iaculis suscipit dui. Nam sit amet sem. Aliquam libero nisi, imperdiet at, tincidunt nec, gravida vehicula, nisl.
---
+++ pelicanconf.py
@@ -0,0 +1,75 @@
+AUTHOR = 'emkeyen'
+SITENAME = 'Mykhail Stieblov'
+SITEURL = 'http://127.0.0.1:8000/'
+TIMEZONE = 'Europe/Kyiv'
+DEFAULT_LANG = 'en'
+
+SUBTITLE = 'hello world'
+SUBTEXT = '''My name is Mykhail 🧑🏻‍💻 and I enjoy transforming ideas into technical solutions. 
+I'm a tech enthusiast who loves automating and testing things. I program in Python, TypeScript and Bash, am a Linux fan, and currently learning Golang. Welcome to my technical blog.
+'''
+COPYRIGHT = '©2025'
+PATH = 'content'
+THEME = 'themes/papyrus'
+THEME_STATIC_PATHS = ['static']
+PLUGIN_PATHS = ['pelican-plugins']
+PLUGINS = ['readtime', 'search', 'neighbors', 'pelican-toc']
+STATIC_PATHS = [
+    'images',
+    'images/favicon.ico',
+    'extra/robots.txt',
+    ]
+EXTRA_PATH_METADATA = {
+    'extra/robots.txt': {'path': 'robots.txt'},
+    'images/favicon.ico': {'path': 'favicon.ico'},
+    }
+DISPLAY_PAGES_ON_MENU = True
+DIRECT_TEMPLATES = (('index', 'search', 'tags', 'categories', 'archives',))
+PAGINATED_TEMPLATES = {'index': None, 'tag': None, 'category': None, 'author': None, 'archives': 24,}
+
+# Site search plugin
+SEARCH_MODE = "output"
+SEARCH_HTML_SELECTOR = "main"
+# Table of Content Plugin
+TOC = {
+    'TOC_HEADERS'       : '^h[1-3]', # What headers should be included in
+                                     # the generated toc
+                                     # Expected format is a regular expression
+    'TOC_RUN'           : 'true',    # Default value for toc generation,
+                                     # if it does not evaluate
+                                     # to 'true' no toc will be generated
+    'TOC_INCLUDE_TITLE': 'false',    # If 'true' include title in toc
+}
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+RSS_FEED_SUMMARY_ONLY = True
+
+# Social widgets
+SOCIAL = (
+    ('github', 'https://github.com/emkeyen96/'),
+    ('telegram', 'https://t.me/emkeyen'),
+    ('linkedin', 'https://linkedin.com/in/emkeyen'),
+)
+
+# Article share widgets
+SHARE = (
+    ("twitter", "https://twitter.com/intent/tweet/?text=Features&amp;url="),
+    ("linkedin", "https://www.linkedin.com/sharing/share-offsite/?url="),
+    ("reddit", "https://reddit.com/submit?url="),
+    ("facebook", "https://facebook.com/sharer/sharer.php?u="),
+    ("whatsapp", "https://api.whatsapp.com/send?text=Features - "),
+    ("telegram", "https://telegram.me/share/url?text=Features&amp;url="),
+)
+
+DEFAULT_PAGINATION = 8
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+# DISQUS_SITENAME = ''
+# GOOGLE_ANALYTICS = ''
